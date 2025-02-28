b21e475c36751da9047efcf35d23cb180610abcf
dhclql0106 <dhclql010661@gmail.com>
2025-02-28 11:21:01 +0900

    .

---
+++ index.js
@@ -0,0 +1,8 @@
+function dp_menu() {
+  let click = document.getElementById("drop-menu");
+  if (click.style.display === "none") {
+    click.style.display = "block";
+  } else {
+    click.style.display = "none";
+  }
+}
